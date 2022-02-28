80614afb2f131fae6fa7720da7c607d8914724b0
aslanmagamaev <aslanmagamaev@mail.ru>
2022-03-01 02:20:35 +0300

    0.1. FileOutputStream. [#252488 #270171]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,20 +1,29 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.util.Arrays;
 
 public class ResultFile {
     public static void main(String[] args) {
+        int[][] matrix = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
+            }
+        }
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("1, 2, 3, 4, 5".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("2, 4, 6, 8, 10".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("3, 6, 9, 12, 15".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("4, 8, 12, 16, 20".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("5, 10, 15, 20, 25".getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    String s;
+                    if (j == 0) {
+                        s = "" + matrix[i][j];
+                    } else {
+                        s = ", " + matrix[i][j];
+                    }
+                    out.write(s.getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
