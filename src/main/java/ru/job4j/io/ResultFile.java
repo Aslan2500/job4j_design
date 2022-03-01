6d34dd1ff0b5a717a3bb570c1ae2c76285bab49a
aslanmagamaev <aslanmagamaev@mail.ru>
2022-03-01 18:49:11 +0300

    0.1. FileOutputStream. [#252488 #270171]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,27 +5,30 @@ import java.util.Arrays;
 
 public class ResultFile {
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                matrix[i][j] = (i + 1) * (j + 1);
-            }
-        }
+        int[][] matrix = multiple(10);
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                    String s;
+            for (int[] ints : matrix) {
+                for (int j = 0; j < ints.length; j++) {
                     if (j == 0) {
-                        s = "" + matrix[i][j];
+                        System.out.print("" + ints[j]);
                     } else {
-                        s = ", " + matrix[i][j];
+                        System.out.print(", " + ints[j]);
                     }
-                    out.write(s.getBytes());
                 }
-                out.write(System.lineSeparator().getBytes());
+                System.out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static int[][] multiple(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return matrix;
     }
 }
