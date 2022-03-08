cbf3017edec3ecbe47ace7c7c0e412c17325ea75
aslanmagamaev <aslanmagamaev@mail.ru>
2022-03-08 14:21:42 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            try (FileOutputStream out = new FileOutputStream(target)) {
+                boolean flag = false;
+                String s;
+                while ((s = in.readLine()) != null) {
+                    String[] rsl = s.split(" ");
+                    if ("400".equals(rsl[0]) || "500".equals(rsl[0])) {
+                        flag = true;
+                        out.write(rsl[1].getBytes());
+                    }
+                    if (("200".equals(rsl[0]) || "300".equals(rsl[0]) && flag)) {
+                        out.write(rsl[1].getBytes());
+                        flag = true;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
