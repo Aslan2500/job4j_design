de201ae765d2950058bf3d838ca394604a1b8b44
aslanmagamaev <aslanmagamaev@mail.ru>
2022-03-10 22:11:36 +0300

    4.1. Сканирование файловой системы. [#106929 #273808]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,15 +1,11 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class Search {
     public static void main(String[] args) throws IOException {
@@ -18,10 +14,8 @@ public class Search {
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        List<Path> rsl = new ArrayList<>();
-        if (condition.test(root.getFileName())) {
-            rsl.add(Files.walkFileTree(root, new PrintFiles()));
-        }
-        return rsl;
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
