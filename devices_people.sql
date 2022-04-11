e48ab76604627674ab268285a6b95c6e6dbb4f4e
Aslan Magamaev <94488647+Aslan2500@users.noreply.github.com>
2022-04-11 20:33:33 +0300

    2. Группировка и агрегатные функции [#373618 #287263]

--- devices_people.sql
+++ devices_people.sql
@@ -25,23 +25,29 @@ insert into people (name) values ('Ivan');
 insert into people (name) values ('Alex');
 insert into people (name) values ('Lev');
 
-insert into devices_people (device_id, people_id) values (1, 1), (1, 4);
+insert into devices_people (device_id, people_id) values (1, 1);
+insert into devices_people (device_id, people_id) values (1, 4);
 insert into devices_people (device_id, people_id) values (2, 3);
 insert into devices_people (device_id, people_id) values (3, 2);
 insert into devices_people (device_id, people_id) values (3, 3);
-insert into devices_people (device_id, people_id) values (4, 1), (4, 2);
+insert into devices_people (device_id, people_id) values (4, 1);
+insert into devices_people (device_id, people_id) values (4, 2);
 
 select avg(price) from devices;
 
 select p.name, avg(d.price)
 from devices d
+join devices_people dp
+on d.id = dp.device_id
 join people p
-on p.id = d.id
+on p.id = dp.people_id
 group by p.name;
 
 select p.name, avg(d.price)
 from devices d
+join devices_people dp
+on d.id = dp.device_id
 join people p
-on p.id = d.id
+on p.id = dp.people_id
 group by p.name
-having avg(d.price) > 5000;
+having avg(d.price) > 5000; 
